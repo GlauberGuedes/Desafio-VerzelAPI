0a0589f7f4ccb1d20249f1f003ee8c8adb942966
glauberguedes <glauber-94@hotmail.com>
2021-06-30 21:26:45 -0300

    implementando página de editar módulo

---
+++ controladores/modulos.js
@@ -0,0 +1,82 @@
+const knex = require("../conexao");
+
+const listasModulos = async (req, res) => {
+  try{
+    const modulos = await knex('modulos');
+
+    res.json(modulos);
+  }catch(error) {
+    return res.status(400).json(error.message)
+  }
+}
+
+const cadastrarModulo = async (req, res) => {
+  const { nome } = req.body;
+
+  if(!nome) {
+    res.status(400).json('O campo nome é obrigatório.')
+  }
+
+  try {
+    const modulo = await knex('modulos').insert({ nome }).returning('*');
+
+    if(!modulo) {
+      res.status(400).json('Não foi possível cadastrar o módulo.')
+    }
+
+    res.status(200).json(modulo);
+  }catch(error) {
+    return res.status(400).json(error.message)
+  }
+}
+
+const atualizarModulo = async (req, res) => {
+  const { nome} = req.body;
+  const { id } = req.params;
+
+  if(!nome) {
+    return res.status(400).json('O campo nome é obrigatório.');
+  }
+
+  try{
+    const modulo = await knex('modulos').where({id: id});
+
+    if(!modulo) {
+      return res.status(404).json('Módulo não encontrado');
+    }
+
+    const moduloAtualizado = await knex('modulos').update({nome}).where({id});
+
+    if(moduloAtualizado === 0) {
+      return res.status(400).json('Não foi possível atualizar o módulo.');
+    }
+
+    res.json('Módulo atualizado com sucesso.')
+  }catch(error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+const deletarModulo = async (req, res) => {
+  const { id } = req.body;
+
+  try{
+    const modulo = await knex('modulos').where({id: id});
+
+    if(!modulo) {
+      return res.status(404).json('Módulo não encontrado');
+    }
+
+    const moduloDeletado = await knex('modulos').del().where({id: id});
+
+    if(moduloDeletado === 0) {
+      return res.status(400).json('Não foi possível deletar o módulo.');
+    }
+
+    return res.json('Módulo deletado com sucesso.')
+  }catch(error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+module.exports = { cadastrarModulo, listasModulos, atualizarModulo, deletarModulo }
--- rotas.js
+++ rotas.js
@@ -2,7 +2,8 @@ const express = require('express');
 const rotas = express();
 const cadastro = require('./controladores/cadastro');
 const login = require('./controladores/login');
-//const verificarToken = require('./filtros/verificarToken');
+const verificarToken = require('./filtros/verificarToken');
+const modulo = require('./controladores/modulos');
 
 //cadastro
 rotas.post('/cadastro', cadastro.CadastrarUsuario);
@@ -10,8 +11,12 @@ rotas.post('/cadastro', cadastro.CadastrarUsuario);
 //login
 rotas.post('/login', login.loginUsuario);
 
-//rotas.use(verificarToken);
+rotas.use(verificarToken);
 
+rotas.get('/modulos', modulo.listasModulos);
+rotas.post('/modulos', modulo.cadastrarModulo);
+rotas.put('/modulos/:id', modulo.atualizarModulo);
+rotas.delete('/modulos', modulo.deletarModulo);
 
 
 module.exports = rotas;
