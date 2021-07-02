7007807f4ca2d1a2b801bf52540f45681344be18
glauberguedes <glauber-94@hotmail.com>
2021-07-02 16:45:46 -0300

    banco de dados heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,14 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-      host: 'localhost',
-      user: 'postgres',
-      password: 'postgres',
-      database: 'verzel_bd'
+      host: 'ec2-52-6-77-239.compute-1.amazonaws.com',
+      user: 'hnuopsugrjelul',
+      password: 'f32538acd45607722c141ab88f411bd8aeb2f00f52ab439b0055b22432fd27f4',
+      database: 'd28m5m94uscb00',
+      port: 5432,
+      ssl: {
+        rejectUnauthorized: false
+      }
   }
 });
 
